370533693ec8d06d8643f42cd1f7ed06d862773a
Kevin <75491985+khdebruijn@users.noreply.github.com>
2026-07-10 16:45:12 +0200

    removing font_size parameter from industry

--- src/coastal_dynamics/industry.py
+++ src/coastal_dynamics/industry.py
@@ -19,7 +19,7 @@ class QuestionIndustry:
         serve: Serves a pn.column concatenating multiple questions.
     """
 
-    def __init__(self, question_dict, font_size="16px"):
+    def __init__(self, question_dict):
         self.question_dict = question_dict
         self.font_size = font_size
 
